59f8deb21085249cbfd4d2973b8577a20750dea7
Tomasz <75432078+tpyrzak@users.noreply.github.com>
2021-04-03 01:00:35 +0200

    initial commit

---
+++ src/com/javastart/TypyDanychCw1.java
@@ -0,0 +1,16 @@
+package com.javastart;
+
+public class TypyDanychCw1 {
+
+    public static void main(String[] args) {
+	int age = 25;
+	boolean isTrue = true;
+	char initialN = 'J';
+	char initialS = 'K';
+	boolean isTrueInitials = true;
+
+	System.out.printf("Cześć Jan, słyszałem, że masz %d lat i uczysz sie programowania. Czy to prawda?\n" +
+				"-%b\n-Czy Twoje inicjały to %c%c?\n-Tak,%b",age,isTrue,initialN,initialS,isTrueInitials);
+
+    }
+}
---
+++ src/com/javastart/TypyDanychCw2.java
@@ -0,0 +1,11 @@
+package com.javastart;
+
+public class TypyDanychCw2 {
+    public static void main(String[] args) {
+        System.out.println("Mam Audi A4");
+        System.out.println("Rocznik:");
+        System.out.println(2004);
+        System.out.println("Silnik o pojemności:");
+        System.out.println(2.0);
+    }
+}
